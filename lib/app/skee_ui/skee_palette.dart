60894e2546cfe83c01bb06e2631e0d2469ffb63a
Flávio Melo <hastnumber1@gmail.com>
2022-11-02 06:52:45 -0300

    Create a new background color
    Create a splash screen background

--- lib/app/skee_ui/skee_palette.dart
+++ lib/app/skee_ui/skee_palette.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class WEPalette {
   static const primaryColor = Colors.teal;
   static final formBackgroudColor = Colors.grey.shade200;
+  static const splashBackgroundColor = Color.fromARGB(255, 68, 66, 66);
 }
