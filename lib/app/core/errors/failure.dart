01bd10228f70ea46c6f5dc7bebfbb969a2184bb4
Romolo Nobre <romolo.nobre@shift.online>
2022-12-21 18:15:34 +0000

    rename fileds

--- lib/app/core/errors/failure.dart
+++ lib/app/core/errors/failure.dart
@@ -1,19 +1,19 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 abstract class IFailure implements Exception {
-  final String message;
+  final String? errorMessage;
 
   IFailure({
     StackTrace? stackTrace,
     dynamic exception,
-    required this.message,
+    required this.errorMessage,
   });
 }
 
 class Failure extends IFailure {
   @override
-  String message;
+  String errorMessage;
   dynamic exception;
   StackTrace? stackTrace;
-  Failure({this.message = 'Unkwon message', this.stackTrace, this.exception})
-      : super(message: message, stackTrace: stackTrace, exception: exception);
+  Failure({this.errorMessage = 'Unkwon message', this.stackTrace, this.exception})
+      : super(errorMessage: errorMessage, stackTrace: stackTrace, exception: exception);
 }
