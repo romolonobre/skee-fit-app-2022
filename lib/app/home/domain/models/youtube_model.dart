8d2c6d9e51716e3f6ba0b4d5532e3af98bcdb4a3
Romolo Nobre <romolo.nobre@shift.online>
2022-12-30 14:42:24 +0000

    crete model

---
+++ lib/app/home/domain/models/youtube_model.dart
@@ -0,0 +1,53 @@
+class YoutubeModel {
+  final String id;
+  final String title;
+  final String profilePictureUrl;
+  final String subscriberCount;
+  final String videoCount;
+  final String uploadPlaylistId;
+  List<Video>? videos;
+
+  YoutubeModel({
+    required this.id,
+    required this.title,
+    required this.profilePictureUrl,
+    required this.subscriberCount,
+    required this.videoCount,
+    required this.uploadPlaylistId,
+    this.videos,
+  });
+
+  factory YoutubeModel.fromMap(Map<String, dynamic> map) {
+    return YoutubeModel(
+      id: map['id'],
+      title: map['snippet']['title'],
+      profilePictureUrl: map['snippet']['thumbnails']['default']['url'],
+      subscriberCount: map['statistics']['subscriberCount'],
+      videoCount: map['statistics']['videoCount'],
+      uploadPlaylistId: map['contentDetails']['relatedPlaylists']['uploads'],
+    );
+  }
+}
+
+class Video {
+  final String? id;
+  final String? title;
+  final String? thumbnailUrl;
+  final String? channelTitle;
+
+  Video({
+    this.id,
+    this.title,
+    this.thumbnailUrl,
+    this.channelTitle,
+  });
+
+  factory Video.fromMap(Map<String, dynamic> snippet) {
+    return Video(
+      id: snippet['resourceId']['videoId'],
+      title: snippet['title'],
+      thumbnailUrl: snippet['thumbnails']['high']['url'],
+      channelTitle: snippet['channelTitle'],
+    );
+  }
+}
