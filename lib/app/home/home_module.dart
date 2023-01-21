80b8e95739cda9c7799ccd4b87f18d22b2dbc8e7
Romolo Nobre <romoloferreira@icloud.com>
2023-01-21 12:35:29 +0000

    remove unused files

--- lib/app/home/home_module.dart
+++ lib/app/home/home_module.dart
@@ -6,17 +6,11 @@ import 'package:skeefiapp/app/home/service/get_youtube_videos_service.dart';
 import 'package:skeefiapp/app/home/service/image_picker_service.dart';
 import 'package:skeefiapp/app/my_exercises/my_exercies_module.dart';
 
-import 'presenter/cubit/get_fitness_news_cubit.dart';
 import 'presenter/home_screen.dart';
-import 'repositories/get_fitness_news_repository.dart';
-import 'service/get_fitness_news_service.dart';
 
 class HomeModule extends Module {
   @override
   List<Bind<Object>> get binds => [
-        Bind.lazySingleton<GetFitnessNewsRepository>((i) => GetFitnessNewsRepositoryImpl()),
-        Bind.lazySingleton<GetFitnessNewsService>((i) => GetFitnessNewsServiceImpl(repository: i())),
-        Bind.lazySingleton((i) => GetFitnessNewsCubit(i())),
         Bind.lazySingleton((i) => ImagepickerCubit()),
         Bind.lazySingleton<ImagePickerService>((i) => ImagePickerServiceImpl()),
         Bind.lazySingleton<GetYoutubeVideosRepository>((i) => GetYoutubeVideosRepositoryImpl()),
