208de3a50c433f8ed014fb076c257ca3d9ab7dc3
Romolo Nobre <romoloferreira@icloud.com>
2023-01-24 19:14:50 +0000

    create DetailsBottomsheet

---
+++ lib/app/exercises/presenter/widgets/details_bottomsheet.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../../../widgets/skee_modal.dart';
+import '../../domain/models/exercises_model.dart';
+
+class DetailsBottomsheet {
+  static Future openExercisesdetail(BuildContext context, ExercisesModel exercise) async {
+    SkeeModal(
+      context,
+      height: 300,
+      cancelText: 'Done',
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.white,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.network(
+                exercise.gifUrl,
+                height: 190,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
