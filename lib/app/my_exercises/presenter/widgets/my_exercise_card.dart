019918639a5b0086ab81b150d9f9e4bcf836ff6e
Romolo Nobre <romoloferreira@icloud.com>
2023-01-24 18:40:43 +0000

    create CustomDimissibleWidget

--- lib/app/my_exercises/presenter/widgets/my_exercise_card.dart
+++ lib/app/my_exercises/presenter/widgets/my_exercise_card.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -6,6 +7,7 @@ import '../../../exercises/presenter/widgets/exercise_card_widget.dart';
 import '../../../widgets/skee_button.dart';
 import '../../../widgets/skee_modal.dart';
 import '../../../widgets/skee_text.dart';
+import 'custom_dimissible_widget.dart';
 
 class MyExercisesBody extends StatefulWidget {
   final List<ExercisesModel> exercises;
@@ -30,32 +32,7 @@ class _MyExercisesBodyState extends State<MyExercisesBody> {
         itemBuilder: (context, index) {
           final exercise = widget.exercises[index];
 
-          return Dismissible(
-            key: UniqueKey(),
-            direction: DismissDirection.endToStart,
-            background: Container(
-              width: 20,
-              padding: const EdgeInsets.symmetric(
-                horizontal: 8,
-                vertical: 15,
-              ),
-              margin: const EdgeInsets.only(top: 8),
-              decoration: BoxDecoration(
-                color: Colors.red.shade400,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: const [
-                  Icon(
-                    Icons.delete_outline_outlined,
-                    size: 50,
-                    color: Colors.grey,
-                  ),
-                ],
-              ),
-            ),
-            onDismissed: (direction) => setState(() => widget.exercises.removeAt(index)),
+          return CustomDimissibleWidget(
             child: ExerciseCardWidget(
               exercise: exercise,
               trailing: SkeeButton.iconButton(
@@ -68,6 +45,7 @@ class _MyExercisesBodyState extends State<MyExercisesBody> {
               ),
               ontap: () => setState(() => exercise.isDone = !exercise.isDone),
             ),
+            onDismissed: (direction) => setState(() => widget.exercises.removeAt(index)),
           );
         },
       ),
