bb3f1c4d6a2379787eec7ec7534debf1f2bee70f
Romolo Nobre <romoloferreira@icloud.com>
2023-01-27 20:03:42 +0000

    remove unused methods

--- lib/app/my_exercises/presenter/widgets/my_exercise_card.dart
+++ lib/app/my_exercises/presenter/widgets/my_exercise_card.dart
@@ -1,13 +1,10 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 
 import '../../../exercises/domain/models/exercises_model.dart';
 import '../../../exercises/presenter/widgets/exercise_card_widget.dart';
 import '../../../widgets/skee_button.dart';
-import '../../../widgets/skee_modal.dart';
-import '../../../widgets/skee_text.dart';
 import 'custom_dimissible_widget.dart';
 
 class MyExercisesBody extends StatefulWidget {
@@ -52,41 +49,6 @@ class _MyExercisesBodyState extends State<MyExercisesBody> {
             onDismissed: (direction) => setState(() => widget.exercises.removeAt(index)),
           );
         },
-      ),
-    );
-  }
-
-  bool _isWorkoutCompleted() {
-    return widget.exercises.every((element) => element.isDone == true);
-  }
-
-  Future<void> _openWorkOutCompletedModal({required Function ontap}) async {
-    SkeeModal(
-      context,
-      onCancel: () => ontap(),
-      onConfirm: () => Modular.to.pushNamed('/home'),
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 20),
-        height: 180,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SkeeText.title('Well done!', fontsize: 15),
-            const SizedBox(height: 14),
-            SkeeText.custom(
-              "You have completed your workout.",
-              fontsize: 15,
-              color: Colors.grey,
-              fontWeight: FontWeight.bold,
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
       ),
     );
   }
