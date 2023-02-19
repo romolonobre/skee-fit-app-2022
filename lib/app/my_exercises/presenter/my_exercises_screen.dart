47845b22e48761de8080b1e711472545404b9d3d
Romolo Nobre <romoloferreira@icloud.com>
2023-02-19 12:14:17 +0000

    test

--- lib/app/my_exercises/presenter/my_exercises_screen.dart
+++ lib/app/my_exercises/presenter/my_exercises_screen.dart
@@ -33,10 +33,6 @@ class _MyExercisesPageState extends State<MyExercisesPage> {
         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
           return [
             SliverAppBar(
-              elevation: 0,
-              pinned: true,
-              snap: true,
-              floating: true,
               bottom: const PreferredSize(
                 preferredSize: Size(0, 120),
                 child: WorkoutTimerWidget(),
